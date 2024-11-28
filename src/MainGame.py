aa529d4fd000385562f419795a5c4131cc946e42
Sh0ookshed <ralphnewsonsmith@icloud.com>
2024-11-28 20:49:06 +0000

    feat: created gameloop + setup and imported libraries

--- src/MainGame.py
+++ src/MainGame.py
@@ -1,1 +1,20 @@
+#File that the main game will run in
 
+#Libraries
+import pygame
+from sys import *
+
+#global variables
+
+#setup
+window = pygame.display.set_mode((800,800))
+pygame.display.set_caption("Placeholder")
+run = True
+
+#Gameloop
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit
+            exit()
+    pygame.display.update()
